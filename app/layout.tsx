618c58c3f1d7fc898df8a595ed5ea58bd7fa170a
Sushant Sharma <itsofficialsushant@gmail.com>
2026-03-29 02:33:32 +0530

    Fevicon Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,19 +24,18 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/nepallogo.svg',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/nepallogo.svg',
         media: '(prefers-color-scheme: dark)',
       },
       {
-        url: '/icon.svg',
+        url: '/nepallogo.svg',
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
   },
 }
 
