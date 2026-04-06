36392251db8e495a0c6b5e641d260aa220f0070b
Sushant Sharma <itsofficialsushant@gmail.com>
2026-04-07 01:33:41 +0530

    Changes Saving

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,9 @@
-"use server"
+// /lib/db.ts
+import mysql from "mysql2/promise";
 
-import { neon } from "@neondatabase/serverless"
-
-const sql = neon(process.env.DATABASE_URL!)
-
-export { sql }
+export const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "Sush@nt.2004",
+  database: "aura_db",
+});
